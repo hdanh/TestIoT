46d2b2e0230690958fa1a28314efb81d04339996
Danh Hoang <danh.hoang1991@gmail.com>
2020-09-13 17:50:32 +0700

    Add project files.

---
+++ TestIoT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing.Text;
+using System.Threading;
+using Iot.Device.CpuTemperature;
+
+namespace TestIoT
+{
+    internal class Program
+    {
+        private static CpuTemperature temperature = new CpuTemperature();
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (temperature.IsAvailable)
+                {
+                    Console.WriteLine($"CPU Temp: {temperature.Temperature.Celsius}");
+                }
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
